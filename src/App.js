91c116e47b6b6d9eeec57d7eec82c77fb7815a3b
Dieter <dieter.halosta@yahoo.com>
2020-11-20 18:33:06 +0200

    Added a sign in form with a custom button

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import './App.css';
 import Header from './components/header/header.component';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
+import AuthPage from './pages/auth-page/auth.component'
+
 
 function App() {
   return (
@@ -14,6 +16,7 @@ function App() {
       <Switch>
         <Route exact path='/' component={HomePage}/>
         <Route path='/shop' component={ShopPage}/>
+        <Route path='/signin' component={AuthPage}/>
       </Switch>
     </div>
   );
---
+++ src/pages/auth-page/auth.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './auth.style.scss';
+
+import SignIn from '../../components/sign-in/sign-in.component';
+
+const AuthPage = () => (
+    <div className='sign-in-and-sign-up'>
+        <SignIn />
+    </div>
+)
+
+export default AuthPage;
