069a72c987833b87fb4d41c2bb19c353a20a68e9
Dieter <dieter.halosta@yahoo.com>
2020-11-20 09:07:00 +0200

    Fixed image URLs

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -10,32 +10,32 @@ class Directory extends React.Component{
             sections: [
                 {
                     title: 'ever greens',
-                    imageUrl: 'https://www.pexels.com/photo/photo-of-green-leaves-3571551/',
+                    imageUrl: 'https://images.pexels.com/photos/3571551/pexels-photo-3571551.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
                     id: 1,
                     linkUrl: 'shop/evergreens'
                 },
                 {
                     title: 'plants with flowers',
-                    imageUrl: 'https://www.pexels.com/photo/fresh-flowers-in-glass-jar-at-sunlight-5764075/',
+                    imageUrl: 'https://images.pexels.com/photos/5764075/pexels-photo-5764075.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
                     id: 2,
                     linkUrl: 'shop/flowers'
                 },
                 {
                     title: 'exotics',
-                    imageUrl: 'https://unsplash.com/photos/O1YwZvJNHRw',
+                    imageUrl: 'https://images.unsplash.com/photo-1561627213-f689b52ccebf?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1782&q=80',
                     id: 3,
                     linkUrl: 'shop/exotics'
                 },
                 {
                     title: 'indoor plants',
-                    imageUrl: 'https://www.pexels.com/photo/potted-green-indoor-plants-3076899/',
+                    imageUrl: 'https://images.pexels.com/photos/3076899/pexels-photo-3076899.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
                     size: 'large',
                     id: 4,
                     linkUrl: 'shop/indoor'
                 },
                 {
                     title: 'outdoor plants',
-                    imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
+                    imageUrl: 'https://images.pexels.com/photos/298246/pexels-photo-298246.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
                     size: 'large',
                     id: 5,
                     linkUrl: 'shop/outdoor'
