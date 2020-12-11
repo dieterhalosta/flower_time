9765c8421e3831416e9691f3ba5896ba11165b74
Dieter <dieter.halosta@yahoo.com>
2020-12-11 15:25:51 +0200

    Moved shop data to firebase

--- src/App.js
+++ src/App.js
@@ -10,16 +10,15 @@ import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import AuthPage from './pages/auth-page/auth.component';
 import CheckoutPage from './pages/checkout/checkout.component';
-import {auth,createUserProfileDocument} from './firebase/firebase.utils';
+import {auth,createUserProfileDocument,addCollectionAndDocuments} from './firebase/firebase.utils';
+import {selectCollectionsForPreview} from './redux/shop/shop.selectors';
 
 class App extends React.Component {
-
-
   unsubscribeFromAuth = null;
 
   componentDidMount(){
 
-    const {setCurrentUser} = this.props;
+    const {setCurrentUser, collectionsArray} = this.props;
 
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
      if(userAuth){
@@ -33,6 +32,7 @@ class App extends React.Component {
        })
      }
      setCurrentUser(userAuth);
+     addCollectionAndDocuments('collections', collectionsArray.map(({title, items}) => ({title, items})));
     })
   }
 
@@ -65,7 +65,8 @@ class App extends React.Component {
 }
 
 const mapStateToProps = createStructuredSelector({
-  currentUser: selectCurrentUser
+  currentUser: selectCurrentUser,
+  collectionsArray: selectCollectionsForPreview
 })
 
 
