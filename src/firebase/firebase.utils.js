3488b27d254c1d02ccc1ca065634a20d5c67e53a
Dieter <dieter.halosta@yahoo.com>
2020-12-15 14:13:53 +0200

    Fixed getting data from firebase. Working on introducint with Spinner component

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -54,6 +54,25 @@ const config = {
     return await batch.commit();
   }
 
+  export const convertCollectionsSnapshotToMap = (collections) => {
+    const convertCollectionsSnapshotToMap = collections.docs.map(doc => {
+      const {title, items} = doc.data();
+
+      return {
+        routeName: encodeURI(title.toLowerCase()),
+        id: doc.id,
+        title,
+        items
+      }
+    });
+
+    return convertCollectionsSnapshotToMap.reduce ((accumulator, collection) => {
+      accumulator[collection.title.toLowerCase()] = collection;
+      return accumulator;
+    } ,{});
+  }
+
+
   export const auth = firebase.auth();
   export const firestore = firebase.firestore();
 
