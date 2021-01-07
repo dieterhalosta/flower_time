fdddce9903b9269d7c5089705d59c8306dd9b3c0
Dieter <dieter.halosta@yahoo.com>
2021-01-07 14:54:56 +0200

    Managed to add data to firebase with code

--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -9,11 +9,13 @@ export const selectCollections = createSelector(
 
 export const selectCollectionsForPreview = createSelector(
     [selectCollections],
-    collections => collections ? Object.keys(collections).map(key => collections[key]) : []
-);
+    //collections => collections ? Object.keys(collections).map(key => collections[key]) : []
+    collections => Object.keys(collections).map(key => collections[key])
+    );
 
 export const selectCollection = collectionUrlParam => 
     createSelector (
         [selectCollections],
-        collections => (collections ? collections[collectionUrlParam] : null)
+        //collections => (collections ? collections[collectionUrlParam] : null)
+        collections => collections[collectionUrlParam]
     );
