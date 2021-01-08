9128d652700d02ee746d18958c9dd14f3fc9b4d0
Dieter <dieter.halosta@yahoo.com>
2021-01-08 14:43:26 +0200

    Getting data from Firestore to shop reducer

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -22,16 +22,23 @@ componentDidMount(){
     const {updateCollections} = this.props;
     const collectionRef = firestore.collection('collections');
     
-/*  collectionRef.get().then((snapshot) => {
-        const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
-        updateCollections(collectionsMap);
-    }); */
+    // this.unsubscribeFromSnapshot = collectionRef.onSnapshot(async snapshot => {
+    //     const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
+    //     updateCollections(collectionsMap);
+    //     this.setState({loading:false});
 
-    this.unsubscribeFromSnapshot = collectionRef.onSnapshot(async snapshot => {
+
+    collectionRef.get().then(snapshot => {
         const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
         updateCollections(collectionsMap);
         this.setState({loading:false});
     })
+
+    //Using Firebase API access to DB
+    /*fetch('https://firestore.googleapis.com/v1/projects/flower-time/databases/(default)/documents/collections')
+    .then(response => response.json())
+    .then(collections => console.log(collections));*/
+
 }
 
 render (){
