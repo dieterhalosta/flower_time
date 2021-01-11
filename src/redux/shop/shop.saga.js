921165e09b9e748ec69de064b501c4eb2151d5ba
Dieter <dieter.halosta@yahoo.com>
2021-01-11 17:01:06 +0200

    Implementing Shop Saga and store saga

--- src/redux/shop/shop.saga.js
+++ src/redux/shop/shop.saga.js
@@ -1,10 +1,23 @@
-import {takeEvery} from 'redux-saga/effects';
+import {takeLatest, call, put} from 'redux-saga/effects';
 import ShopActionTypes from './shop.types';
+import {firestore, convertCollectionsSnapshotToMap} from '../../firebase/firebase.utils';
+import {
+    fetchCollectionsSuccess,
+    fetchCollectionsFailure
+} from './shop.actions'
 
-export function* fetchCollectionsAsync(){
-    yield console.log("I am fire");
+
+export function* fetchCollections(){
+   try{
+    const collectionRef = firestore.collection('collections');
+    const snapshot = yield collectionRef.get();
+    const collectionsMap = yield call (convertCollectionsSnapshotToMap, snapshot);
+    yield put(fetchCollectionsSuccess(collectionsMap));
+} catch (error) {
+    yield put(fetchCollectionsFailure(error.message));
+   }
 }
 
-export function* fetchCollectionsStart(){
-    yield takeEvery(ShopActionTypes.FETCH_COLLECTIONS_START, fetchCollectionsAsync);
+export function* onFetchCollectionsStart(){
+    yield takeLatest(ShopActionTypes.FETCH_COLLECTIONS_START, fetchCollections);
 }
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,9 @@
 import {createStore, applyMiddleware} from 'redux';
 import {persistStore} from 'redux-persist';
 import logger from 'redux-logger';
-import rootReducer from './root-reducer';
 import createSagaMiddleware from 'redux-saga';
-import {fetchCollectionsStart} from './shop/shop.saga';
+import rootReducer from './root-reducer';
+import rootSaga from './root-saga';
 
 const sagaMiddleware = createSagaMiddleware();
 
@@ -15,7 +15,7 @@ if (process.env.NODE_ENV === 'development') {
 
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
-sagaMiddleware.run(fetchCollectionsStart);
+sagaMiddleware.run(rootSaga);
 
 export const persistor = persistStore(store);
 
