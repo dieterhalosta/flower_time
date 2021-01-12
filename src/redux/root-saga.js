56f56a15a17ff90c0282820764050facebe67857
Dieter <dieter.halosta@yahoo.com>
2021-01-12 17:54:34 +0200

    implemented shop sagas

--- src/redux/root-saga.js
+++ src/redux/root-saga.js
@@ -1,10 +1,12 @@
 import {all, call} from 'redux-saga/effects';
-import {onFetchCollectionsStart} from './shop/shop.saga';
+import {shopSagas} from './shop/shop.saga';
 import {userSagas} from './user/user.saga';
+import {cartSagas} from './cart/cart.saga';
 
 export default function* rootSaga(){
     yield all([
-        call(onFetchCollectionsStart),
-        call(userSagas)
+        call(shopSagas),
+        call(userSagas),
+        call(cartSagas)
     ])
 }
--- src/redux/shop/shop.saga.js
+++ src/redux/shop/shop.saga.js
@@ -1,4 +1,4 @@
-import {takeLatest, call, put} from 'redux-saga/effects';
+import {takeLatest, call, put, all} from 'redux-saga/effects';
 import ShopActionTypes from './shop.types';
 import {firestore, convertCollectionsSnapshotToMap} from '../../firebase/firebase.utils';
 import {
@@ -20,4 +20,8 @@ export function* fetchCollections(){
 
 export function* onFetchCollectionsStart(){
     yield takeLatest(ShopActionTypes.FETCH_COLLECTIONS_START, fetchCollections);
+}
+
+export function* shopSagas(){
+    yield all([call(onFetchCollectionsStart)])
 }
