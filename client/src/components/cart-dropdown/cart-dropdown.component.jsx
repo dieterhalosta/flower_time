a394828837d05fa5e54b9bff1cf1ffc1858f5f19
Dieter <dieter.halosta@yahoo.com>
2021-01-25 16:06:10 +0200

    separated code from client and back end to serve stripe payments

--- client/src/components/cart-dropdown/cart-dropdown.component.jsx
+++ client/src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -17,7 +17,7 @@ const CartDropdown = ({cartItems, history, dispatch}) => (
                 <CartItem key={cartItem.id} item={cartItem}/>
             ))
             :
-            <span className='empty-message'>Your cart is emplty</span>
+            <span className='empty-message'>Your cart is empty</span>
             }                    
         </div>
         <CustomButton onClick={() => {
